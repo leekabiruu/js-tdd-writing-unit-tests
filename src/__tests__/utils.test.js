3506dc90ab9108977c48879883e462ef8e3438f1
Lee <leekinyuakabiru@gmail.com>
2025-07-28 09:25:47 +0300

    done

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,12 @@
 // Your tests here
+import { pointsForWord } from "../utils";
+
+describe("pointsForWord", () => {
+  it("calculates total points (1 per vowel, 2 per consonant)", () => {
+    expect(pointsForWord("test")).toBe(7);
+  });
+
+  it("handles uppercase letters", () => {
+    expect(pointsForWord("tEsT")).toBe(7);
+  });
+});
